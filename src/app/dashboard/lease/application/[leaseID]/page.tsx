81069c3d0ba898bebad9e17296f5025c4720ef2a
enpyth <enpyth@outlook.com>
2025-07-19 15:06:13 +0930

    fix: update leaseId to leaseID

--- src/app/dashboard/lease/application/[leaseID]/page.tsx
+++ src/app/dashboard/lease/application/[leaseID]/page.tsx
@@ -2,16 +2,16 @@ import { getLeaseById } from "@/db/queries/leases";
 import FormReviewPage from "@/features/lease-application/form-review-page";
 
 type PageProps = {
-  params: { leaseId: string } | Promise<{ leaseId: string }>;
+  params: { leaseID: string } | Promise<{ leaseID: string }>;
 };
 
 export default async function LeaseFormPage({ params }: PageProps) {
   console.log("params", params);
-  const { leaseId } = await params;
-  const leaseIdInt = parseInt(leaseId);
+  const { leaseID } = await params;
+  const leaseIdInt = parseInt(leaseID);
 
   if (isNaN(leaseIdInt)) {
-    throw new Error(`Invalid leaseId: ${leaseId}`);
+    throw new Error(`Invalid leaseID: ${leaseID}`);
   }
 
   const leaseData = await getLeaseById(leaseIdInt);
